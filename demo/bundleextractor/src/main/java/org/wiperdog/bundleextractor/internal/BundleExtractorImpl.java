fa0fb0b5687256eab1deffb75aaebc06411b667b
leminhquan <leminhquan@leminhquan-virtual-machine.(none)>
2014-04-22 16:40:32 +0700

    update get status and rewrite resource file For first run and only first run. Increase performance for next run

--- demo/bundleextractor/src/main/java/org/wiperdog/bundleextractor/internal/BundleExtractorImpl.java
+++ demo/bundleextractor/src/main/java/org/wiperdog/bundleextractor/internal/BundleExtractorImpl.java
@@ -1,15 +1,22 @@
 package org.wiperdog.bundleextractor.internal;
 
+import java.util.List;
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
 import java.util.jar.JarEntry;
 import java.util.jar.JarFile;
 
@@ -31,45 +38,50 @@ public class BundleExtractorImpl implements BundleExtractor {
 		return listResources;
 	}
 
+	@SuppressWarnings({ "unchecked", "rawtypes" })
 	public boolean processListResources() {
-		try {
-			String line;
-			BufferedReader br = new BufferedReader(new FileReader(listResources));
-			while((line = br.readLine()) != null){
-				String location = processResource(line);
+		// Read listResource file for list bundle to get and extract
+		List<Map> listBundle = readResourceFile(listResources);
+		
+		// Process get and extract bundles
+		for (Map bundle : listBundle) {
+			String location = processResource(bundle);
+			if(location != null && !"".equals(location)){
 				File gotBundle = new File(location);
 				System.out.println(gotBundle.getName() + ":" + gotBundle.exists());
 				extractPackage(gotBundle);
 			}
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
+			bundle.put("getit", false);
 		}
+		
+		// Rewrite listResource file for updating get status
+		rewriteFile(listBundle);
 		return true;
 	}
 
-	public String processResource(String line) {
-		String[] values = line.split(",");
-		String groupId = values[0];
-		String artifactId = values[1];
-		String version = values[2];
-		String location = System.getProperty("felix.home") + values[3];
-		String repo = values[4];
-		if(!"".equals(groupId) && !"".equals(artifactId) && !"".equals(version) && !"".equals(location)){
-			//Write and execute maven command to get bundle from maven repository
-			StringBuffer sb = new StringBuffer();
-			sb.append("mvn org.apache.maven.plugins:maven-dependency-plugin:2.8:get ");
-			sb.append("-Dartifact=" + groupId+ ":" + artifactId +":" + version + " ");
-			sb.append("-Ddest=" + location);
-			if(!"".equals(repo)){
-				sb.append(" -DrepoUrl=" + repo);
-			}	
-			String result = executeCommand(sb.toString());
-			if(!"".equals(result)){
-				System.out.println(result);
-//				return artifactId + "-" + version + ".jar";
-				return location;
+	public String processResource(Map bundle) {
+		boolean getit = (Boolean) bundle.get("getit");
+		if(getit){
+			String groupId = (String) bundle.get("groupId");
+			String artifactId = (String) bundle.get("artifactId");
+			String version = (String) bundle.get("version");
+			String location = System.getProperty("felix.home") + (String) bundle.get("location");
+			String repo = (String) bundle.get("repo");
+			if(!"".equals(groupId) && !"".equals(artifactId) && !"".equals(version) && !"".equals(location)){
+				//Write and execute maven command to get bundle from maven repository
+				StringBuffer sb = new StringBuffer();
+				sb.append("mvn org.apache.maven.plugins:maven-dependency-plugin:2.8:get ");
+				sb.append("-Dartifact=" + groupId+ ":" + artifactId +":" + version + " ");
+				sb.append("-Ddest=" + location);
+				if(!"".equals(repo)){
+					sb.append(" -DrepoUrl=" + repo);
+				}	
+				String result = executeCommand(sb.toString());
+				if(!"".equals(result)){
+					System.out.println(result);
+	//				return artifactId + "-" + version + ".jar";
+					return location;
+				}
 			}
 		}
 		return null;
@@ -106,7 +118,6 @@ public class BundleExtractorImpl implements BundleExtractor {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
 	}
 
 	public String executeCommand(String cmd) {
@@ -125,6 +136,59 @@ public class BundleExtractorImpl implements BundleExtractor {
 			e.printStackTrace();
 		}
 		return output.toString();
+	}
+	
+	/**
+	 * Rewrite to update get status 
+	 * @param listBundle
+	 */
+	public void rewriteFile(List<Map> listBundle){
+		try {
+			FileWriter fw = new FileWriter(listResources.getAbsoluteFile());
+			BufferedWriter bw = new BufferedWriter(fw);
+			StringBuffer sb = new StringBuffer();
+			for (Map bundle : listBundle) {
+				for (Object key : bundle.keySet()) {
+					sb.append(bundle.get(key) + ",");
+				}
+			}
+			bw.write(sb.toString());
+			bw.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * Read listResource file for list bundles to get and extract
+	 * listResource file has format : groupID, artifactID, version, destination, repoUrl, get status(true/false)
+	 * @param listResource
+	 * @return list bundles
+	 */
+	@SuppressWarnings("unchecked")
+	public List<Map> readResourceFile(File listResource){
+		List<Map> listBundle = new ArrayList<Map>();
+		try {
+			BufferedReader br = new BufferedReader(new FileReader(listResource));
+			String line;
+			while((line = br.readLine()) != null){
+				String[] values = line.split(",");
+				Map bundle = new LinkedHashMap();
+				bundle.put("groupId", values[0]);
+				bundle.put("artifactId", values[1]);
+				bundle.put("version", values[2]);
+				bundle.put("location", values[3]);
+				bundle.put("repo", values[4]);
+				bundle.put("getit", values[5] != null ? Boolean.valueOf(values[5]) : true);
+				listBundle.add(bundle);
+			}
+			br.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return listBundle;
 	}
 
 	public void setListResources(File listResources) {
